3c44c4f3b321a6b3007f8269a17df685e01af524
dilobar00 <dilobarmahmudjonova@gmail.com>
2024-02-17 10:29:44 +0300

    init

---
+++ main.js
@@ -0,0 +1,284 @@
+//  "1" 
+ let age =; //son kiriting
+
+if (age > 0) {
+  console.log(age+1);
+} else {
+  console.log(age);
+}
+
+
+
+
+
+
+/*  "2" 
+ let age =;    //son kiriting
+
+if (age > 0) {
+  console.log(age+1);
+} else {
+  console.log(age-2);
+}
+*/
+
+
+
+
+
+/*   "3" 
+ let age =-8;
+
+if(age>0){
+    console.log(age+1);
+} else if(age<0) {
+    console.log(age-2);
+} else {
+    console.log(10);
+} 
+*/
+
+
+
+
+
+/*  "4"
+let a = -5;
+let b = 4;
+let c = -5;
+let count =0;
+
+if(a>0){
+    count++
+}else if(a<0){
+    count;
+}
+if(c>0){
+    count++;
+}else if(c<0){
+   count;
+}
+if(b>0){
+    count++;
+}else if(b<0){
+  count;
+}
+console.log(count,"ta musbat");
+*/
+
+
+
+
+
+
+
+/*  "5"
+let a = -5;
+let b = 4;
+let c = -5;
+let count =0;
+let manfiy =0;
+if(a>0){
+    count++
+}else if(a<0){
+    manfiy++;
+}
+if(c>0){
+    count++;
+}else if(c<0){
+    manfiy++;
+}
+if(b>0){
+    count++;
+}else if(b<0){
+  manfiy++;
+}
+console.log(count,"ta musbat");
+console.log(manfiy,"ta manfiy");
+
+/*  "5 2-usul"
+let son1 = 3;
+let son2 = -2;
+let son3 = 5;
+
+if (son1 > 0) {
+    console.log(son1);
+} else{
+    console.log(son1,"manfiy son")
+}
+if (son2 > 0) {
+    console.log(son2);
+}else{
+    console.log(son2,"manfiy son")
+}
+if (son3 > 0) {
+    console.log(son3);
+}else{
+    console.log(son3,"manfiy son")
+}*/
+
+
+
+
+
+
+/* "6"
+let A =
+5;
+let B =2;
+if (A> B) {
+  console.log(A);
+} else {
+  console.log(B);}
+
+  */
+
+
+/* "7"
+
+
+  let A =
+6;
+let B =1;
+if (A<B) {
+  console.log(A);
+} else {
+  console.log(B);}
+
+  */
+
+
+
+
+
+/* "8"
+let A=3;
+let B=19;
+
+if (A>B) {
+  console.log(A);
+}
+else if (A<B) {
+  console.log(B);
+}
+*/
+
+
+
+
+
+
+
+/*"9"
+let a=7;
+let b=5
+if (a>b) {
+ console.log(a-2); 
+ console.log(b+2);
+}
+else{
+  console.log(a-1);
+  console.log(b+2);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*"10"
+let a=2;
+let b=2;
+if (a!=b) {
+  console.log(a+b);
+}
+else if (a==b) {
+  console.log(0);
+}
+console.log(a,"a ni qiymati");
+console.log(b,"b ni qiymati");
+*/
+
+
+
+
+
+
+/* "11"
+let a=3;
+let b=3;
+if (a>b) {
+  console.log(a);
+}
+
+else if (a<b) {
+  console.log(b);
+}
+else if (a==b){
+console.log(0);
+}
+console.log(a,"a ni qiymati");
+console.log(b,"b ni qiymati");
+*/
+
+
+
+
+
+
+
+/* "12"
+
+let a=3;
+let b=2;
+let c=9;
+if (a>b) {
+  console.log(b);
+}
+else if (b>c) {
+  console.log(c);
+}
+else if(a<c) {
+  console.log(a);
+}*/
+
+
+
+
+
+
+
+/* "13"
+
+let a = 98;
+let b = 8;
+let d = 4;
+if(a>b && a>d && b>d ){
+    console.log(b);
+}
+if(a>b && a>d && d>b ){
+    console.log(d);
+}
+
+if(b>a && b>d && a>d){
+    console.log(a);
+}
+if(b>a && b>d && d>a){
+    console.log(d);
+}
+
+if(d>a && d>b && a>b){
+    console.log(a);
+}
+
+if(d>a && d>b && b>a){
+    console.log(b);
+}
+*/
